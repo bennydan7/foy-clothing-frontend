3a48ecc97977247ce891e3ee2cc6f0eb57b9000e
bennydan7 <owusub243@gmail.com>
2024-03-03 20:35:13 +0000

    Initial commit

---
+++ src/scenes/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ShoppingList from "./ShoppingList";
+
+const Home = () => {
+  return (
+    <div>
+      <ShoppingList />
+    </div>
+  );
+};
+
+export default Home;
